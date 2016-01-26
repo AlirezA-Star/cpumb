f28eecfbd1776cc9d00d947a6bc41dbe99ce6ae1
alireza <alirezakhirandish@gmail.com>
2016-01-26 15:57:41 +0330

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -6,11 +6,11 @@ do
  
  
    local text = matches[1]
-   local chat = "chat#id"..YourChatId 
-   --like : local chat = "chat#id"..12345678
+   local chat = "chat#id"..109844348 
+   --like : local chat = "chat#id"..109844348
    
   local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'Sent!'
+  return 'Sent!💜💌'
  
  end
  end
